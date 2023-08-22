34b232a0dd919393de348e2637a2c764bbcf3b59
Alejandro Castro <acastro.ma1@gmail.com>
2023-08-21 20:47:51 -0400

    commit de java

---
+++ spring-boot-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Juan', 'Pérez', 'juan.perez@example.com', '2023-04-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('María', 'García', 'maria.garcia@example.com', '2023-04-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Luis', 'Hernández', 'luis.hernandez@example.com', '2023-04-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Laura', 'Martínez', 'laura.martinez@example.com', '2023-04-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Jorge', 'González', 'jorge.gonzalez@example.com', '2023-04-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ana', 'Rodríguez', 'ana.rodriguez@example.com', '2023-04-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Pedro', 'Sánchez', 'pedro.sanchez@example.com', '2023-04-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Marta', 'Pérez', 'marta.perez@example.com', '2023-04-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Javier', 'Fernández', 'javier.fernandez@example.com', '2023-04-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Sofía', 'Gutiérrez', 'sofia.gutierrez@example.com', '2023-04-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Carlos', 'Gómez', 'carlos.gomez@example.com', '2023-04-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Lucía', 'Martín', 'lucia.martin@example.com', '2023-04-11');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('David', 'García', 'david.garcia@example.com', '2023-04-11');
+
